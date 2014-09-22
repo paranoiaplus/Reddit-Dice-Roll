d9c85903fea64ba2513b49ce4fe8bf0ca6d6d046
Paul Vorobyev <paul.vorobyev@gmail.com>
2014-09-22 19:24:11 -0400

    Bug fixes and improvements

--- README.md
+++ README.md
@@ -1,17 +1,8 @@
 # Reddit Dice Roll Bot
 Decided a Reddit bot would be a fun way of getting reacquainted with Python syntax!
- 
-
-Usage
----
-Fill out the variables that at the top of main.py (i.e. USERNAME, PASSWORD, SUBREDDIT, USER_AGENT) and run with
-
-    python main.py
-
 
 
 
 Thanks
 ---
-* [PRAW](https://github.com/praw-dev/praw)
-* My mother
+[PRAW](https://github.com/praw-dev/praw)
--- main.py
+++ main.py
@@ -2,19 +2,16 @@
 
 import praw
 import random
+from config import *
 
 #Config Variables
-USERNAME = ''
-PASSWORD = ''
-SUBREDDIT = ''
-USER_AGENT = ''
+
 
 ############
 
 #Setup Variables
 r = praw.Reddit(user_agent=USER_AGENT)
 r.login(USERNAME, PASSWORD)
-all_comments = r.get_comments(SUBREDDIT)
 completedRolls = set()
 
 #############
@@ -23,7 +20,10 @@ def determineRollInfo(commentBody):
     commandLocation = commentBody.find("!Roll ")
     startOfRoll = commandLocation + 6 #Location where roll starts
     endOfRoll = commentBody[startOfRoll:].find(" ") + startOfRoll #Location where roll ends
-    rollString = commentBody[startOfRoll:endOfRoll]
+    if endOfRoll - startOfRoll is -1:
+        rollString = commentBody[startOfRoll:]
+    else:
+        rollString = commentBody[startOfRoll:endOfRoll]
     numberOfDice = int(rollString[0:rollString.find("d")])
     numberOfSides = int(rollString[-(len(rollString) - 1 - len(str(numberOfDice))):]) #OH GOD WHY
     infoArray = [numberOfDice, numberOfSides]
@@ -35,12 +35,13 @@ def resultingRoll(infoArray):
     for i in range (0, infoArray[0]):
         newRoll = 1 + round((random.random() * infoArray[1]))
         rollToReturn += newRoll
-        print "Roll " + str(i) + " out of " + str(infoArray[0]) + " = " + str(newRoll)
+        print "Roll " + str(i + 1) + " out of " + str(infoArray[0]) + " = " + str(newRoll)
     return rollToReturn
 
 
 
 def scanComments():
+    all_comments = r.get_comments(SUBREDDIT)
     for comment in all_comments:
         if "!Roll" in comment.body and comment.id not in completedRolls:
             replyString = ''
@@ -50,9 +51,10 @@ def scanComments():
             replyString += "You're rolling " + str(infoArray[0]) + " dice with " + str(infoArray[1]) + " sides each.\n"
             print("Rolling....")
             replyString += "\nYour resulting roll is: **" + str(resultingRoll(infoArray)) + "**!\n"
-            replyString += "\n---\n^(PM /u/paranoiaplus for any suggestions or recommendations.)"
+            replyString += "\n---\nPM /u/paranoiaplus for any suggestions or recommendations. Source code found [here](https://github.com/paranoiaplus/Reddit-Dice-Roll-Bot/)."
             comment.reply(replyString)
             print("Comment made!")
+            print("\nSearching...\n")
             completedRolls.add(comment.id)
     
 
@@ -64,6 +66,7 @@ while True:
         scanComments()
     except Exception as e:
         print("Error: ", str(e))
+
 
 
 
